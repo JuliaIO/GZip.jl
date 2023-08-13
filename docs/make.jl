36ea6f50675fdb8c87c647584fea96e19d07fd97
Viral B. Shah <ViralBShah@users.noreply.github.com>
2023-08-13 17:04:04 -0400

    save

--- docs/make.jl
+++ docs/make.jl
@@ -3,16 +3,11 @@ using Documenter
 
 DocMeta.setdocmeta!(GZip, :DocTestSetup, :(using GZip); recursive = true)
 
-makedocs(;
+makedocs(
+    sitename = "GZip.jl",
     modules = [GZip],
     authors = "JuliaIO and contributors",
-    repo = "https://github.com/JuliaIO/GZip.jl/blob/{commit}{path}#{line}",
-    sitename = "GZip.jl",
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://JuliaIO.github.io/GZip.jl",
-        assets = String[],
-    ),
+    format = Documenter.HTML(; assets = String[]),
     pages = [
         "Home" => "index.md",
     ],
--- src/GZip.jl
+++ src/GZip.jl
@@ -1,5 +1,43 @@
 ## gzip file io
 
+"""
+GZip.jl: A Julia interface for gzip functions in zlib
+
+This module provides a wrapper for the gzip related functions of
+([zlib](http://zlib.net/)), a free, general-purpose, legally
+unencumbered, lossless data-compression library. These functions allow
+the reading and writing of gzip files.
+
+## Notes
+
+-   This interface is only for gzipped files, not the streaming zlib
+    compression interface. Internally, it depends on/uses the
+    streaming interface, but the gzip related functions are higher
+    level functions pertaining to gzip files only.
+-   `GZipStream` is an implementation of `IO` and can be used virtually
+    anywhere `IO` is used.
+-   This implementation mimics the `IOStream` implementation, and should be a
+    drop-in replacement for `IOStream`, with some caveats:
+    -   `seekend` and `truncate` are not available
+    -   `readuntil` is available, but is not very efficient. (But `readline` works fine.)
+
+In addition to [`open`](@ref), [`gzopen`](@ref), and [`gzdopen`](@ref), the
+following `IO`/`IOStream` functions are supported:
+
+-   `close()`
+-   `flush()`
+-   `seek()`
+-   `skip()`
+-   `position()`
+-   `eof()`
+-   `read()`
+-   `readuntil()`
+-   `readline()`
+-   `write()`
+-   `peek()`
+
+Due to limitations in `zlib`, `seekend` and `truncate` are not available.
+"""
 module GZip
 
 using Libdl
