57cff17fe791e6befdb544922fecc0601144c513
Viral B. Shah <ViralBShah@users.noreply.github.com>
2023-08-14 18:36:22 -0400

    Update docs (#102)

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,14 @@
 using GZip
 using Documenter
 
-push!(LOAD_PATH,"../src/")
-
 makedocs(
     sitename = "GZip.jl",
     modules = [GZip],
     authors = "JuliaIO and contributors",
     format = Documenter.HTML(; assets = String[]),
     pages = [
-        "Home" => "index.md",
+        "GZip" => "index.md",
+        "Reference" => "reference.md",
     ],
 )
 
